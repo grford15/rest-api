4a5d4966f3a86f17cd1caf2e8cd6a16fb5136687
Greg <g.rutherford10@gmail.com>
2019-07-30 20:42:09 +0100

    add new routes

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config');
+
+const postRoute = require('./routes/posts');
+
+app.use('/posts', postRoute);
 
 app.get('/', (req, res) => {
   res.send('We are home!');
 });
 
-app.get('/posts', (req, res) => {
-  res.send('We are on posts');
-});
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true },
+  () => console.log('Connected to DB'),
+);
 
 app.listen(3000, () => console.log('app is listening on port 3000'));
