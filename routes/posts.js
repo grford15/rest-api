b0b68673851d3d3d261d4f7c9643e5eecf477f5f
Greg <g.rutherford10@gmail.com>
2019-07-30 20:51:22 +0100

    add post router

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const Post = require('../models/Post');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('We are on posts');
+});
+
+router.post('/', (req, res) => {
+  console.log(req.body);
 });
 
 module.exports = router;
